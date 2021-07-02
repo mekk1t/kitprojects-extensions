b07689a57b9b0d2bd2801a82b29f82c233c4d389
mekk1t <kit.kalimov@gmail.com>
2021-07-02 23:25:18 +0300

    Класс расширений для дженериков.

---
+++ PrimitiveTypesExtensions/PrimitiveTypesExtensions/GenericExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace KitProjects.PrimitiveTypes.Extensions
+{
+    /// <summary>
+    /// Класс расширений с применением дженериков.
+    /// </summary>
+    public static class GenericExtensions
+    {
+        /// <summary>
+        /// Возвращает <see langword="null"/> вместо значения по умолчанию для значимых типов.
+        /// </summary>
+        /// <typeparam name="T">Значимый тип объекта.</typeparam>
+        public static T? ToNullIfDefault<T>(this T obj) where T : struct
+        {
+            if (EqualityComparer<T>.Default.Equals(obj, default))
+                return null;
+            else
+                return obj;
+        }
+    }
+}
